59ce7668349a2c6688ff41f6137ad2c0c5919d0f
Kael Mitsuhiko <Kael Mitsuhiko>
2017-03-24 22:02:18 +0800

    Updated.

--- PseudoStack.java
+++ PseudoStack.java
@@ -36,4 +36,29 @@ public class PseudoStack<T>{
 		}
 		return items.peek();
 	}
+	
+	public String toString() {
+		String ret = "", element = "";
+		PseudoQueue<T> buffer = new PseudoQueue<>();
+		for(int i = 0; i < size; i++) {
+			buffer.enqueue(items.dequeue());
+		}
+		
+		PseudoStack<T> buffer2 = new PseudoStack<>();
+		for(int i = 0; i < size; i++) {
+			T t = buffer.dequeue();
+			buffer2.push(t);
+			items.enqueue(t);
+		}
+		
+		for(int i = 0; i < size; i++) {
+			T t = buffer2.pop();
+			element = String.valueOf(t);
+			ret += "|" + element + "|";
+		}
+		buffer = null;
+		return ret;
+	}
+	
+	
 }
